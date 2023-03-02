13614ecb520e79c5a54bacb7ccc72bbe03871d1c
zzunho9 <jamlove4763@naver.com>
2023-03-03 00:10:06 +0900

    Save staged changes to a text file with git diff command

--- src/main.rs
+++ src/main.rs
@@ -11,25 +11,16 @@ struct Cli {
 fn main() {
     // let args = Cli::parse();
 
-    // Get the latest two commits hash
-    let output = Command::new("git").arg("log").arg("-2").arg(r#"--pretty=format:"%h""#).output().unwrap();
-    if !output.status.success() {
-        panic!("git log failed");
-    }
-    let hash = String::from_utf8(output.stdout).unwrap();
-    let mut lines = hash.lines();
-    let (from, to) = (lines.next().unwrap(), lines.next().unwrap());
-    println!("{from} {to}");
-
-    // Get the diff between the two commits and save it to a text file
+    // // Get the diff between the two commits and save it to a text file
     let file_name= format!("diff-{}.txt", chrono::Local::now().timestamp());
     let output = Command::new("git")
-        .args(& ["diff", "1419b34", "bb1792e", "", &file_name])
+        .args(& ["diff", "--staged"])
         .output().unwrap();
 
     if !output.status.success() {
         panic!("git diff failed: {}", String::from_utf8(output.stderr).unwrap());
     }
-}
 
-// fatal: ambiguous argument ''1419b34'': unknown revision or path not in the working tree.
+    let diff = String::from_utf8(output.stdout).unwrap();
+    std::fs::write(&file_name, diff).unwrap();
+}
