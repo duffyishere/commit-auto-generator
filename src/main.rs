1419b344139de4d8afa8af75ff70df2e5172fdb5
zzunho9 <jamlove4763@naver.com>
2023-03-02 21:35:09 +0900

    test commit

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,10 @@ struct Cli {
 
 fn main() {
     // let args = Cli::parse();
-
-    let output = Command::new("git").arg("log").arg("-2").arg(r#"-pretty=format:"%h""#).output().unwrap();
+    let output = Command::new("git").arg("log").arg("-2").arg(r#"--pretty=format:"%h""#).output().unwrap();
     if !output.status.success() {
         println!("err");
-        // error_chain::bail!("Command executed with failing error code");
     }
-    let stdout = String::from_utf8(output.stdout).unwrap();
-    let lines = stdout.lines();
-    for line in lines {
-        println!("{}", line);
-    }
+    let hashVec = String::from_utf8(output.stdout).unwrap();
+    let hashVec: Vec<&str> = hashVec.split_whitespace().collect();
 }
